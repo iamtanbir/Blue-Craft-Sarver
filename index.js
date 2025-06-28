61d5deab7b4defa92b45c9f3188c42938cc9c6d8
iamtanbir <mdtanbirahmed271116@gmail.com>
2025-06-28 19:42:25 +0600

    deploy sarver

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-  origin: ["http://localhost:5173", "https://blue-craft-auth.firebaseapp.com", "https://blue-craft-auth.firebasestorage.app", "https://blue-crafts-client-go5tusnlq-md-tanbir-ahmeds-projects.vercel.app/"],
+  origin: ["http://localhost:5173", "https://blue-craft-auth.firebaseapp.com", "https://blue-craft-auth.firebasestorage.app"],
   })
 )
 
